2aa07827058b0cbc3a3e4b05b9fd322b3150adb4
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2024-05-08 01:21:48 +0800

    Update UVa 11933 splittingnumbers.cpp

--- UVa 11933 splittingnumbers.cpp
+++ UVa 11933 splittingnumbers.cpp
@@ -1,1 +1,25 @@
 // UVa 11933
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    unsigned int N;
+    while (cin >> N && N != 0) {
+        int a = 0, b = 0;
+        long long multiplier = 1;
+        bool flag = true; // true means a, false means b
+
+        while (N > 0) {
+            int bitmask = N & 1;
+            if (bitmask==1) {
+            if (flag) (a += multiplier);
+            else (b+= multiplier);
+            flag = !flag;
+            }
+            multiplier *=2;
+            N >>=1;    
+        }
+        cout << a << " " << b << endl;
+    }
+    return 0;
+}
